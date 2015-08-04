084240eaf6654ecde6d81b82860da7a97f47c4cf
Jeremy Singer <jeremy.singer@glasgow.ac.uk>
2015-08-04 17:28:55 +0100

    Logfile parser to transform into CSV files

---
+++ scripts/logfile2csv.py
@@ -0,0 +1,11 @@
+# logfile2csv.py
+# Jeremy Singer
+# 4 Aug 2015
+
+# simple Python script to transform
+# textual log dump from tinylog.txt
+# into a CSV file for weka
+
+# Part of Victor's summer intern project
+
+
